c0083af540d1d391b92c3636c0c05835fbefc355
turbomaxee <137450788+TurboMaxe@users.noreply.github.com>
2026-04-04 11:28:24 +0500

    fix

--- common-kotlin/src/main/kotlin/io/turbo/clientblockkotlin/ClientBlock.kt
+++ common-kotlin/src/main/kotlin/io/turbo/clientblockkotlin/ClientBlock.kt
@@ -1,12 +1,15 @@
-package io.turbo.cli
+package io.turbo.clientblockkotlin
 
+import io.turbo.clientblockkotlin.listeners.PlayerListener
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class ClientBlock : JavaPlugin() {
-    
+
+
     override fun onEnable() {
-        instance = this;
-        logger.info("Enabled ClientBlock (kotlin) v1.0.0")
+        instance = this
+        logger.info("Enabled ClientBlock (kotlin)")
         Bukkit.getPluginManager().registerEvents(PlayerListener(), this)
     }
 
